9447524475c7d10dc16511ed4ffc69efaa781733
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-01 01:25:40 +0300

    Задние 1-4

---
+++ 1 Основы /44 Преобразование к числу /task44.1.js
@@ -0,0 +1,13 @@
+//Задание 1
+let a = '10', b='20';
+console.log(Number(a) + Number(b));
+
+
+//Задание 2
+alert( Number('2') + Number('3') );//  5
+
+//Задание 3
+alert( 2 + Number('3') );//5
+
+//Задание 4
+alert( '2' + Number('3') );//23
