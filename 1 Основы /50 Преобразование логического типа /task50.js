006c1eae092811c0dd5a4d6e49a1289ae432b0fe
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-01 23:00:34 +0300

    Задние 1-7

---
+++ 1 Основы /50 Преобразование логического типа /task50.js
@@ -0,0 +1,21 @@
+// Задание 1 
+alert(true + 3); // 4
+
+// Задание 2
+alert(true + true); // 2
+
+
+// Задание 3
+alert(true - true); //0
+
+// Задание 4
+alert(true + false); //1
+
+// Задание 5
+alert('1' + true); //1true
+
+// Задание 6 
+alert( String(true) + 1 ); // true1
+
+// Задание 7
+alert( String(true) + Number(true) ); // true1
