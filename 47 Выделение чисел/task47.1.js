4b195932fbbc5f2f3d1c45138bcd825a13ca62c2
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-01 22:43:29 +0300

    Задние 1-3

---
+++ 47 Выделение чисел/task47.1.js
@@ -0,0 +1,12 @@
+//Задание 1
+let a = parseInt('5px');
+let b = parseInt('6px');
+console.log(a+b); 
+
+//Задание 2
+let c = parseFloat('5.5px');
+let d = parseFloat('6.25px');
+console.log(c+d); 
+
+//Задание 3
+console.log(c+d + 'px'); 
