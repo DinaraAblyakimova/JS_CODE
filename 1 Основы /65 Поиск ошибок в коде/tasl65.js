1443efe13b819e8f3ef23500678c9fd0a6e0aad6
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-02 20:01:25 +0300

    Задние 1-13

---
+++ 1 Основы /65 Поиск ошибок в коде/tasl65.js
@@ -0,0 +1,65 @@
+//Задание 1
+let num1 = 1;
+let num2 = 2;
+console.log('сумма: ' + (num1 + num2));
+
+//Задание 2
+let a = 1;
+let b = 2;
+console.log(a + b);
+
+//Задание 3
+let num = '123';
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+console.log(sum);
+
+//Задание 4
+let num3 = String(123);
+console.log((num3[0]));
+
+//Задание 5
+let c = 0;
+console.log(++c);
+
+//Задание 6
+let num4 = String(123);
+console.log(num4.length);
+
+//Задание 7
+let k = 24 * 60 * 60;
+console.log(k);
+
+
+//Задание 8
+let num5 = 123;
+let str1 = String(num5);
+console.log(str1.length);
+
+
+//Задание 9
+let num6 = 123;
+let str2 = String(num6);
+console.log(str2[str2.length - 1]);
+
+//Задание 10
+let num7 = 123;
+let str3 = String(num7);
+console.log(str3.length);
+
+//Задание 11
+let num8 = 123;
+let str4 = String(num8);
+console.log(str4[str4.length - 1]);
+
+
+//Задание 12
+let q = '123';
+let g = '456';
+let s = Number(q) + Number(g);
+console.log(s);
+
+//Задание 13
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+console.log(aaa + bbb + ccc);
