b7e0b510bc3ee5d05516a7eb0eea45ffeaf008a1
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-02 10:50:23 +0300

    Задние 1-9

--- 1 Основы /60 Префиксный и постфиксный тип/task60.js
+++ 1 Основы /60 Префиксный и постфиксный тип/task60.js
@@ -4,46 +4,46 @@ let num = 3;
 alert(++num); // 4
 
 // Задание 2
-let num = 3;
-alert(num++);// 3
+let с = 3;
+alert(с++);// 3
 
 // Задание 3
-let num = 3;
-alert(--num);// 2
+let k = 3;
+alert(--k);// 2
 
 // Задание 4
-let num = 3;
-alert(num--);// 3
+let b = 3;
+alert(b--);// 3
 
 // Задание 5
-let num1 = 3;
-let num2 = ++num1;
-alert(num1);
-alert(num2);// 3   4
+let v = 3;
+let l = ++v;
+alert(v);
+alert(l);// 3   4
 
 // Задание 6
-let num1 = 3;
-let num2 = num1++;
-alert(num1);
-alert(num2);// 3  4
+let q = 3;
+let s = q++;
+alert(q);
+alert(s);// 3  4
 
 
 // Задание 7
-let num1 = 3;
-let num2 = --num1;
-alert(num1);
-alert(num2); // 3   2
+let a = 3;
+let t = --a;
+alert(a);
+alert(t); // 3   2
 
 // Задание 8 
-let num1 = 3;
-let num2 = num1--;
-alert(num1);
-alert(num2); // 3   3
+let m = 3;
+let d = m--;
+alert(m);
+alert(d); // 3   3
 
 
 // Задание 9
-let num1 = 3;
-num1++;
-let num2 = num1--;
-alert(num1++);
-alert(--num2); // 4 2
+let h = 3;
+h++;
+let j = h--;
+alert(h++);
+alert(--j); // 4 2
