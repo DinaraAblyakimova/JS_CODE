28f76b029788fadc0d37d8b7906162ae4cde0f88
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-02 09:55:00 +0300

    Задние 1-3

---
+++ 1 Основы /52 Получение символов строки/task52.js
@@ -0,0 +1,18 @@
+//Задание 1 
+
+let a = 'abcde';
+console.log(a[0]);
+console.log(a[2]);
+console.log(a[4]);
+
+//Задание 2
+
+let  b = 'abcde';
+let k =(b[4] + b[3] + b[2] + b[1] +b[0]);
+console.log(k);
+
+//Задание 3
+let c = 'abcde', num = 3;
+console.log(c[num]);
+
+
