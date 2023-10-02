731a76c65ff0011b654b0de67a396f02ca471483
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-02 10:27:39 +0300

    Задние 1-3

---
+++ 1 Основы /56 Обращение к цифрам числа/task56.js
@@ -0,0 +1,11 @@
+//Задание 1
+let a = String(12345);
+console.log(Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]) + Number(a[4]));
+
+
+//Задание 2
+console.log(Number(a[0]) * Number(a[1]) * Number(a[2]) * Number(a[3]) *  Number(a[4]));
+
+//Задание 3
+console.log((a[4]) + (a[3]) +(a[2]) + (a[1]) + (a[0]));
+
