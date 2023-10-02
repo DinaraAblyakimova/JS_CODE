a9047289d81fea45d4d0772c0ac4afc4a4db0740
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-02 10:46:45 +0300

    Задние 1-9

---
+++ 1 Основы /60 Префиксный и постфиксный тип/task60.js
@@ -0,0 +1,49 @@
+// Задание 1
+
+let num = 3;
+alert(++num); // 4
+
+// Задание 2
+let num = 3;
+alert(num++);// 3
+
+// Задание 3
+let num = 3;
+alert(--num);// 2
+
+// Задание 4
+let num = 3;
+alert(num--);// 3
+
+// Задание 5
+let num1 = 3;
+let num2 = ++num1;
+alert(num1);
+alert(num2);// 3   4
+
+// Задание 6
+let num1 = 3;
+let num2 = num1++;
+alert(num1);
+alert(num2);// 3  4
+
+
+// Задание 7
+let num1 = 3;
+let num2 = --num1;
+alert(num1);
+alert(num2); // 3   2
+
+// Задание 8 
+let num1 = 3;
+let num2 = num1--;
+alert(num1);
+alert(num2); // 3   3
+
+
+// Задание 9
+let num1 = 3;
+num1++;
+let num2 = num1--;
+alert(num1++);
+alert(--num2); // 4 2
