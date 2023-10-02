df0146c3cc1b6f104343111ab310c5fc23d2630d
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-02 10:07:12 +0300

    Задние 1-3

---
+++ 1 Основы /54 Последний символ строки/task54.js
@@ -0,0 +1,11 @@
+    //Задание 1
+    let a = 'fgjdjv';
+    console.log(a[a.length -1]);
+
+    // Задание 2
+    let b = 'fgjdjv';
+    console.log(b[b.length -2]);
+
+    // Задание 3
+    let c = 'fgjdjv';
+    console.log(c[c.length -3]);
