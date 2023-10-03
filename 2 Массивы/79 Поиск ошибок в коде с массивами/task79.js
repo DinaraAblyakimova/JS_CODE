dec283b32e8aebde77e8755bd2a8f53cca42d350
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-04 00:02:02 +0300

    Задние 1-4

---
+++ 2 Массивы/79 Поиск ошибок в коде с массивами/task79.js
@@ -0,0 +1,12 @@
+//Задание 1
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[arr.length-1]);
+//Задание 2
+let arr1 = [1, 2, 3, 4, 5];
+console.log((arr1[1]) + (arr1[2]) +(arr1[3]) + (arr1[4]) + arr1[0]);
+//Задание 3
+let arr2 = [1, 2, 3, 4, 5];
+console.log(arr2.length);
+//Задание 4
+let arr3 = [1, 2, 3, 4, 5];
+console.log(arr3.length);
