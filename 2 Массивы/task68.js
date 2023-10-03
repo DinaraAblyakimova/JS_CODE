e42bd5ebc3a9e300380a8dd32ad2caf48a89e2ab
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-03 23:08:44 +0300

    Задние 1-4

---
+++ 2 Массивы/task68.js
@@ -0,0 +1,20 @@
+// Задание 1
+let arr = [1, 2, 3];
+console.log(arr[0]);
+console.log( arr[1]);
+console.log(arr[2]);
+
+// Задание 2
+let arr1 = [1, 2, 3];
+console.log(arr1[0]);
+console.log( arr1[1]);
+console.log(arr1[2]);
+
+
+// Задание 3
+let arr2 = [1, 2, 3];
+console.log(arr2[0] + arr2[1] + arr2[2]);
+
+// Задание 4
+let arr3 = ['a', 'b', 'c', 'd'];
+console.log(String(arr3[0])+ "+" + String(arr3[1])+ '+' + String(arr3[2])+ '+' + String(arr3[3]));
