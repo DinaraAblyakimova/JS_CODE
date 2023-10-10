8b06d393857946c03394862b95aec2dfca52212d
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-10 20:57:29 +0300

    Задние 1-5

---
+++ 4 Условия/111 Равенство по значению и типу/task111.js
@@ -0,0 +1,49 @@
+// Задание 1
+let test1 = '3';
+let test2 = '3';
+		
+if (test1 == test2) {
+	console.log('+++'); // сработает это 
+} else {
+	console.log('---');
+} 
+
+// Задание 2
+let test3 = '3';
+let test4 = '3';
+		
+if (test3 === test4) {
+	console.log('+++');// сработает это 
+} else {
+	console.log('---');
+}
+
+// Задание 3
+let test5 = 3;
+let test6 = '3';
+		
+if (test5 == test6) {
+	console.log('+++');// сработает это 
+} else {
+	console.log('---');
+}
+
+// Задание 4
+let test7 = 3;
+let test8 = '3';
+		
+if (test7 === test8) {
+	console.log('+++');
+} else {
+	console.log('---'); // сработает это 
+} 
+
+// Задание 5
+let test9 = 3;
+let test10 = 3;
+		
+if (test9 === test10) {
+	console.log('+++'); // сработает это 
+} else {
+	console.log('---');
+}
