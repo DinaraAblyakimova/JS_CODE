154aafcb8fe54900d40f0bef07b54e1b36e7fbbb
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-10 19:27:21 +0300

    Задние 1-4

---
+++ 4 Условия/105 Операторы больше и меньшеОператоры больше и меньше/task105.1.js
@@ -0,0 +1,32 @@
+let test = 10;
+
+//Задание 1
+if (test >10){
+    console.log("Да, больше");
+} else {
+    console.log("Нет, не больше");
+}
+
+
+//Задание 2
+if (test < 10){
+    console.log("Да, меньше");
+} else {
+    console.log("Нет не меньше");
+}
+
+//Задание 3
+if (test >= 10){
+    console.log("Да, больше или равна ");
+} else {
+    console.log("Нет, больше или равна");
+}
+
+//Задание 4
+if (test <= 10){
+    console.log("Да, меньше или равна");
+} else {
+    console.log("Нет не меньше или равна");
+}
+
+
