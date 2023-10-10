6160650c8ada33a3567ac937e8d729b6ef550341
DinaraAblyakimova <ablyakimova.dinara@gmail.com>
2023-10-10 19:58:16 +0300

    Задние 1-2

---
+++ 4 Условия/108 Сравнение переменных/task108.js
@@ -0,0 +1,17 @@
+let test1 = 10;
+let test2 = 20;
+
+//Задание 1
+if (test2 > test1) {
+	console.log(test2); 
+} else {
+	console.log(test1);
+}
+
+//Задание 2
+if (test2 == test1) {
+	console.log("Да, равны"); 
+} else {
+	console.log("Нет, не равны");
+}
+
